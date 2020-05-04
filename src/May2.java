eabeac3dcaf068b664a478ef603e8f295526ba00
ashishEM <ashishem06@gmail.com>
2020-05-04 11:22:40 -0500

    May3

--- src/May2.java
+++ src/May2.java
@@ -10,55 +10,6 @@ public class May2
         int c = numJewelsInStones(b, a);
         System.out.println(c);
     }
-    public boolean isBadVersion(int n)
-    {
-        boolean bad;
-        ArrayList<Integer> badn = new ArrayList<>();
-        badn.add(4);
-        badn.add(5);
-        badn.add(6);
-        badn.add(7);
-        badn.add(8);
-        badn.add(9);
-
-        if(badn.contains(n))
-        {
-            bad = true;
-        }
-        else{
-            bad = false;
-        }
-
-        return bad;
-    }
-
-    public int firstBadVersion(int n) {
-
-        int mid=n/2;
-        int cond=0;
-        int num = 1;
-        boolean way= isBadVersion(mid);
-        if (way)
-        {
-            num=1;
-            cond= mid-1;
-        }
-        else
-        {
-            num=mid+1;
-            cond=n-1;
-        }
-        for(int i =num;i<cond;i++)
-        {
-            if(isBadVersion(i))
-            {
-                return i;
-            }
-        }
-        return 1;
-    }
-
-
 
 
     public static int numJewelsInStones(String J, String S)
@@ -74,10 +25,11 @@ public class May2
                 {
                     count++;
                 }
+
+
             }
         }
         return count++;
     }
-
 }
 
---
+++ src/May3.java
@@ -0,0 +1,33 @@
+public class May3
+{
+    public static void main (String[] args)
+    {
+        String Rn= "asfjk";
+        String Mg = "aggegd";
+        System.out.printf(Rn,Mg);
+    }
+
+    public boolean canConstruct(String ransomNote, String magazine)
+    {
+        char[] Rn = ransomNote.toCharArray();
+        char[] Mg = magazine.toCharArray();
+
+
+        for(int i=1;i<Rn.length;i++)
+        {
+            for (int j=1;j<Mg.length;j++)
+            {
+                if (Rn[i] == Mg[j])
+                {
+
+                    magazine.substring();
+                }
+            }
+        }
+
+
+;
+    }
+
+
+}
